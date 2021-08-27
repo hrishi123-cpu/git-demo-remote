693a5cf81d0bde270873448dd7df31b83f91976e
Hrishikesh Mahajan <64744997+hrishi123-cpu@users.noreply.github.com>
2021-08-27 14:17:23 +0530

    Adding math .py

---
+++ math2.py
@@ -0,0 +1,21 @@
+# Add
+def add(x, y):
+    return x+y
+
+# sub
+
+
+def subtract(x, y):
+    return x-y  # commit on master
+
+# multiply
+
+
+def multiply(x, y):
+    return x*y  # on bug456
+
+# divide
+
+
+def divide(x, y):
+    return x/y  # on master
